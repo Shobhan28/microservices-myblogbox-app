21aa45ec62d8313f0e897113d8cbc2f24f447c91
shobhan28 <shobhan28m@gmail.com>
2023-10-29 20:04:23 +0530

    converted from class to interface to achieve abstraction

--- post/src/main/java/com/myblog/post/service/PostService.java
+++ post/src/main/java/com/myblog/post/service/PostService.java
@@ -1,15 +1,9 @@
 package com.myblog.post.service;
 
-import com.myblog.post.config.RestTemplateConfig;
+
 import com.myblog.post.entity.Post;
 import com.myblog.post.payload.PostDto;
-import com.myblog.post.repository.PostRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.Optional;
-import java.util.UUID;
 
 @Service
 public interface PostService {

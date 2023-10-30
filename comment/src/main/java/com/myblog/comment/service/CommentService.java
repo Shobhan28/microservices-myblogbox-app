1704e5af1f49cf09dfdc669665690fa330e56d01
shobhan28 <shobhan28m@gmail.com>
2023-10-30 16:07:18 +0530

    initial commit

--- comment/src/main/java/com/myblog/comment/service/CommentService.java
+++ comment/src/main/java/com/myblog/comment/service/CommentService.java
@@ -4,7 +4,7 @@ import com.myblog.comment.config.RestTemplateConfig;
 import com.myblog.comment.entity.Comment;
 import com.myblog.comment.payload.Post;
 import com.myblog.comment.repository.CommentRepository;
-import org.springframework.beans.factory.annotation.Autowired;;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,23 +20,24 @@ public class CommentService {
     private RestTemplateConfig restTemplate;
 
 
-    public Comment saveComment(Comment comment){
+    public Comment saveComment(Comment comment) {
 
         //Check whether the post exist if yes the save the comment or else throws exception
 
-       Post post = restTemplate.getRestTemplate().getForObject("http://POST-SERVICE/api/post/" + comment.getPostId(), Post.class);
+        Post post = restTemplate.getRestTemplate().getForObject("http://POST-SERVICE/api/post/getpost/" + comment.getPostId(), Post.class);
 
-       if(post!=null) {
+        if (post != null) {
 
-           String randomCommentId = UUID.randomUUID().toString();
-           comment.setCommentId(randomCommentId);
-           comment.setPostId(post.getId());
-           Comment savedComment = commentRepository.save(comment);
-           return savedComment;
-       }
-       return null;
+            String randomCommentId = UUID.randomUUID().toString();
+            comment.setCommentId(randomCommentId);
+            comment.setPostId(post.getId());
+            Comment savedComment = commentRepository.save(comment);
+            return savedComment;
+
+        } else {
+            return null;
+        }
     }
-
     public List<Comment> getCommentByPostId(String postId){
         List<Comment> comments = commentRepository.findByPostId(postId);
         return comments;

2fd698e85d02772d7587b42140a2fdd46ca7d2ee
shobhan28 <shobhan28m@gmail.com>
2023-10-30 16:02:34 +0530

    initial commit

--- comment/src/main/java/com/myblog/comment/entity/Comment.java
+++ comment/src/main/java/com/myblog/comment/entity/Comment.java
@@ -3,7 +3,6 @@ package com.myblog.comment.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -17,12 +16,12 @@ public class Comment {
     @Id
     private String commentId;
 
-    private String body;
-
-    private String email;
-
     private String name;
-
+    private String email;
+    private String body;
     private String postId;
+
+
+
 
 }
